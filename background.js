95d7cb0e418110e3e54b7e8072d2e40ff1a93c69
Messef <85040583+AkTewari123@users.noreply.github.com>
2024-08-11 16:06:11 -0400

    extension is finished

--- background.js
+++ background.js
@@ -34,10 +34,5 @@ chrome.action.onClicked.addListener(async (tab) => {
       });
     }
   } else if (typeof nextState === "undefined" || nextState === "OFF") {
-    // Remove the CSS file when the user turns the extension off
-    await chrome.scripting.removeCSS({
-      files: ["focus-mode.css"],
-      target: { tabId: tab.id },
-    });
   }
 });
